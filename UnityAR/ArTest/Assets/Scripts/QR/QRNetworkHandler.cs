7802c0edf8aa948918ed0d9ce6fd44b9ef12398c
Barış <46918905+Baris-Yildiz@users.noreply.github.com>
2025-10-22 09:09:16 +0300

    Object loading to scene
    * Object loaded from the QR code url is now loaded into the scene as well

--- UnityAR/ArTest/Assets/Scripts/QR/QRNetworkHandler.cs
+++ UnityAR/ArTest/Assets/Scripts/QR/QRNetworkHandler.cs
@@ -1,5 +1,8 @@
+using GLTFast;
 using System.Collections;
 using System.IO;
+using System.Threading.Tasks;
+using TMPro;
 using UnityEditor.Overlays;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -11,6 +14,7 @@ public class QRNetworkHandler : MonoBehaviour
     QRScanner m_QRScanner;
     GameObject m_QRModelDownloadScreen;
     StateMachine m_StateMachine;
+    GameObject m_LoadedModel;
 
     private void Start()
     {
@@ -18,10 +22,12 @@ public class QRNetworkHandler : MonoBehaviour
         m_QRModelDownloadScreen = GameObject.FindWithTag("QRModelDownloadScreen");
         m_QRModelDownloadScreen.SetActive(false);
         m_StateMachine = GameObject.FindWithTag("StateMachine").GetComponent<StateMachine>();
+        m_LoadedModel = null;
     }
 
     public void StartDownloadingModel()
     {
+        ResetNetworkHandler();
         StartCoroutine(DownloadModel(m_QRScanner.QrCode));
     }
 
@@ -30,9 +36,36 @@ public class QRNetworkHandler : MonoBehaviour
         m_QRModelDownloadScreen.GetComponentInChildren<Slider>().value = www.downloadProgress;
     }
 
+    private void ResetNetworkHandler()
+    {
+        m_LoadedModel = null;
+    }
+
+    private async Task LoadGLBObject(byte[] data)
+    {
+        var gltf = new GltfImport();
+        m_QRModelDownloadScreen.GetComponentInChildren<TextMeshProUGUI>().text = "Loading Model";
+        bool success = await gltf.Load(data);
+
+        if (success)
+        {
+            Transform spawnParent = GameObject.Find("QR").transform; //as an example, download the model onto the qr code
+            success = await gltf.InstantiateMainSceneAsync(spawnParent);
+            if (success)
+            {
+                Debug.Log("Model loaded successfully!");
+                m_LoadedModel = spawnParent.GetChild(0).gameObject;
+                return;
+            }
+        }
+
+        Debug.LogError("Model load failed");
+    }
+
     private IEnumerator DownloadModel(string uri)
     {
         m_QRModelDownloadScreen.SetActive(true);
+        m_QRModelDownloadScreen.GetComponentInChildren<TextMeshProUGUI>().text = "Downloading Model";
         using (UnityWebRequest www = UnityWebRequest.Get(uri))
         {
             www.SendWebRequest();
@@ -53,8 +86,11 @@ public class QRNetworkHandler : MonoBehaviour
                 string path = Path.Combine(Application.persistentDataPath, "test.glb");
                 File.WriteAllBytes(path, www.downloadHandler.data);
                 Debug.Log("File downloaded at path: " + path);
+                Task loadTask = LoadGLBObject(www.downloadHandler.data);
+                yield return loadTask.IsCompleted;
             }
         }
+        
         m_QRModelDownloadScreen.SetActive(false);
         m_StateMachine.SetState(StateMachine.States.IDLE_STATE);
     }
